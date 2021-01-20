02df0386f8f3905305102dd06477fd368e49681b
DavidStewartLDN <davidstewart.work@gmail.com>
2021-01-20 22:29:13 +0000

    Added if statement to skip None types

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ for job_elem in job_elems:
   title_elem = job_elem.find('h2', class_='title')
   company_elem = job_elem.find('div', class_='company')
   location_elem = job_elem.find('div', class_='location')
+  # Skip to next element in job_elems if any of above variables return None type.
+  if None in (title_elem, company_elem, location_elem):
+    continue
   print(title_elem.text.strip())
   print(company_elem.text.strip())
   print(location_elem.text.strip())
